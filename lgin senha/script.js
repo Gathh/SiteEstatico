0701c2021d303db293819d7024270550243ba245
Gathh <125030016+Gathh@users.noreply.github.com>
2023-09-29 21:44:47 -0300

    Add files via upload

---
+++ lgin senha/script.js
@@ -0,0 +1,44 @@
+const campoLogin = document.getElementById("login")
+const campoSenha = document.getElementById("password")
+const campoNovoLogin = document.getElementById("novoLogin")
+const campoNovaSenha = document.getElementById("novaSenha")
+const campoRepSenha = document.getElementById("repSenha")
+
+
+function cadastra() {
+    if (campoNovaSenha.value == campoRepSenha.value) {
+        const usuario = {
+            login: campoNovoLogin.value,
+            senha: campoNovaSenha.value
+        };
+
+        let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+        if (bancoDeDados == null) {
+            bancoDeDados = [];
+        }
+        bancoDeDados.push(usuario);
+        localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
+        alert("Usuario cadastrado com sucesso!");
+
+    } else {
+        alert("Senhas não coincidem");
+    }
+}
+function login() {
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuario ou senha incorreta!";
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+    if (bancoDeDados == null) {
+        mensagem = "Nenhum usuário cadastrado até o momento";
+    } else {
+        for (let usuario of bancoDeDados) {
+            if (usuario.login == login && usuario.senha == senha) {
+                mensagem = "Logado com sucesso!"
+                window.location.href = "./home page/home.html";
+                break;
+            }
+        }
+    }
+    alert(mensagem);
+}
